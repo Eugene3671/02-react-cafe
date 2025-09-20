c08d36a31326c86d27011684059d7b05a2536bd4
Eugene <korzh367@gmail.com>
2025-09-20 18:52:40 +0200

    structur done

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import CareInfo from '../CafeInfo/CafeInfo'
+import css from './App.module.css'
+
+function App() {
+  return (
+    <div className={css.app}>
+      <CareInfo/>
+    </div>
+  )
+}
+
+export default App
